dd21f72eee50cf3ed01197973a0ed6b99b5e98de
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:36:49 +0200

    One million people

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,13 @@ export default function Products() {
   return (
     <div className='max-w-6xl mx-auto'>
         <h1>Products code is added in productions...</h1>
+
+        <div className="">
+             <h1>Product list...</h1>
+             <h2>Product 01</h2>
+             <h2>Product 02</h2>
+             <h2>Product 03</h2>
+        </div>
     </div>
   )
 }
