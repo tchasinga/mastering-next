f690ba5e4d2046782a4e8fe634cbadbef1806446
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:32:42 +0200

    New code ...

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div className='max-w-6xl mx-auto'>
+        <h1>Products code is added in productions...</h1>
+    </div>
+  )
+}
