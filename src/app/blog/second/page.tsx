1044db08a88cea8d37eed955c5b8027bac451d39
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:23:52 +0200

    new code is added

---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Second() {
+  return (
+    <div className='max-w-6xl mx-auto'>
+        <h1>This a good thins for code...</h1>
+    </div>
+  )
+}
