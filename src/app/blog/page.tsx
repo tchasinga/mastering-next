ab043ccd0cc054288ae19a6b12480281202cd988
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:15:57 +0200

    next routing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Blogs() {
   return (
-    <div className='max-w-6xl'>
+    <div className='max-w-6xl mx-auto'>
          <h1> this is my blogs components</h1>
     </div>
   )
