2691a580e15cf577bd84550142588ebd0242d6f8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 13:20:19 +0200

    new code

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {notFound} from "next/navigation"
 
 export default function Reviews({params} : {
     params: {
@@ -6,6 +7,9 @@ export default function Reviews({params} : {
         reviewId: string
     }
 }) {
+   if(parseInt(params.reviewId) > 1000){
+        return notFound()
+   }
   return (
     <div className='max-w-6xl mx-auto'>
         <h1>Review code {params.reviewId} for {params.productId} </h1>
