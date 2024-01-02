1b5a94025d898fa03ef1fe1a1a568e88596a9d21
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 11:54:06 +0200

    New code is added.....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="max-w-7xl mx-auto">
       <h1>Balolewami is learning....</h1>
     </main>
   )
