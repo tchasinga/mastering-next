351058f09e0d79e78ea403a40bbd9aa1a9334f0d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:45:36 +0200

    new code

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function ProductID() {
+export default function ProductID({params}: {params: {productId: string}}) {
   return (
     <div className='max-w-6xl mx-auto'>
-        <h1>Details pages</h1>
+        <h1>Details pages {params.productId}</h1>
     </div>
   )
 }
