35898fdef75ac713a5561bcdcb48b82f85141cc0
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 14:02:31 +0200

    new code online

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className='mybg p-4'>
+           <p>Header</p>
+        </header>
+        {children}
+        <footer className='foot p-4'>
+           <p>Footer</p>
+        </footer>
+        </body>
     </html>
   )
 }
