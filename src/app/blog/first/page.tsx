874e6bdf332d53b09958c7374afd8b4c70067ac9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-02 12:19:44 +0200

    New code ....

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Firstpage() {
+  return (
+    <div className='max-w-6xl mx-auto'>
+         <h1>This my the nexted page</h1>
+    </div>
+  )
+}
