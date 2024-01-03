b2dbf239eaef2888ff4deeb762d14cb9b8f579d5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 12:19:40 +0200

    New code is added

--- src/app/blog/loading.tsx
+++ src/app/blog/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>
-      <h1>Page is Loading...</h1>
+    <div className='max-w-6xl mx-auto'>
+       <h1>Page is Loading...</h1>
     </div>
   )
 }
