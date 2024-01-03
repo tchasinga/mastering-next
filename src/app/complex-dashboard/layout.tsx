15abce1ecea441539e9f862fe3b0bdeefce0284e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 13:12:52 +0200

    New coding added

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function layout({
         <div>{children}</div>
             <UserAnalytics />
             <RevenueMetrics />
-            <Notifications />
+            <Notifications /> 
     </div>
   )
 }
