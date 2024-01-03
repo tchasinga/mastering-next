1009541e3aaeb0661e08fb6d59b8150283ca3ef8
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 10:18:41 +0200

    new code

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AuthLayout({
+    children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className='max-w-6xl mx-auto'>
+        <h1>Inner coding</h1>
+        {children}
+    </div>
+  )
+}
