d3065371652a8c5686bdae0bb3644ff5732958fd
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 10:52:18 +0200

    new code title: params.title

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Metadata } from 'next';
 
-export default function ProductID({params}: {params: {productId: string}}) {
+interface Props {
+  params: { productId: string };
+}
+
+export const generateMetadata = ({ params }: Props): Metadata => ({
+  title: `Product ${params.productId}`,
+});
+
+const ProductID: React.FC<Props> = ({ params }: Props) => {
+  const metadata = generateMetadata({ params });
+
   return (
     <div className='max-w-6xl mx-auto'>
-        <h1>Details pages {params.productId}</h1>
+      <h1>Details pages {params.productId}</h1>
     </div>
-  )
-}
+  );
+};
+
+export default ProductID;
