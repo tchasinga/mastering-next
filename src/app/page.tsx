380d4cce2ed00d09bfbfeba69386a944d02b6f56
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 11:12:19 +0200

    New code is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main className="max-w-7xl mx-auto">
       <h1>Balolewami is learning....</h1>
+
+      <div className="flex gap-0 ">
+        <Link href='./blog'>Blod</Link>
+        <Link href='./about'>About</Link>
+        <Link href='./products'>Products</Link>
+      </div>
     </main>
   )
 }
