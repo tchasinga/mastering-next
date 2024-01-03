fcbab3dc1344f1fe7852eb9296749f114816a733
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 16:00:29 +0200

    new commit

--- src/app/f1/f2/page.tsx
+++ src/app/f1/f2/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function F2() {
   return (
-    <div>
+    <div className='max-w-6xl mx-auto'>
         <h1>this is my second page</h1>
     </div>
   )
--- src/app/f1/page.tsx
+++ src/app/f1/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function F1() {
   return (
-    <div>
+    <div className='max-w-6xl mx-auto'>
         <h1>this is folder one</h1>
+        <div className="">
+          <Link href='/f1/f2'>Go to F2</Link>
+        </div>
     </div>
   )
 }
