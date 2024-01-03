8dae990f5a088063dc99170b875e611252f784f4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 15:55:59 +0200

    new code is added

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function F2() {
+  return (
+    <div>
+        <h1>this is my second page</h1>
+    </div>
+  )
+}
---
+++ src/app/f1/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function F1() {
+  return (
+    <div>
+        <h1>this is folder one</h1>
+    </div>
+  )
+}
