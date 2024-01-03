6861fee091c3b87e353493e999a285578a899779
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 13:23:02 +0200

    new logic is added

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 
 export default function layout({
     children,
+    users,
+    notification,
+    revenue,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  users: React.ReactNode,
+  notification: React.ReactNode,
+  revenue: React.ReactNode,
 }) {
   return (
     <div>
         <div>{children}</div>
-        
+
     </div>
   )
 }
