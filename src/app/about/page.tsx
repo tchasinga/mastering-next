97a7de5b11ac1413ac2b18355d09d1db7a87495c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-03 10:57:16 +0200

    New code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+export const metadata = {
+  title: 'About Next.js Version 14',
+}
+
 export default function About() {
   return (
     <div className='max-w-6xl mx-auto'>
